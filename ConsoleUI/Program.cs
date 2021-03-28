fea141f90dc54faf9945ef9f33017ffc44ab2aad
hachemchrayti <hachemfst@gmail.Com>
2021-03-28 17:47:49 +0200

    Apply dependency injection using Autofac

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using DemoLibrary;
+﻿using Autofac;
+using DemoLibrary;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,21 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            BusinessLogic businessLogic = new BusinessLogic();
 
-            businessLogic.ProcessData();
+            var container = ContainerConfig.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                //var app = scope.Resolve<IApplication>();
+                //app.Run();
+
+                //===Same thing as :
+                var app = scope.Resolve<IBusinessLogic>();
+                app.ProcessData();
+            }
 
             Console.ReadLine();
+
         }
     }
 }
